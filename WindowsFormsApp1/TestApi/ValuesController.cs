5920761d98a83cd45fa4b876f7d4745d112e3f2b
bharadwajralla <bharadwajrallabhandii@gmail.com>
2025-09-03 17:55:30 +0530

    testapi

---
+++ WindowsFormsApp1/TestApi/ValuesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+    }
+}
